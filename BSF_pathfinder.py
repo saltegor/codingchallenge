cf2e9127f0c6e4ea7ec4857bbab0b94df4240a93
Egor Saltykov <saltegor@gmail.com>
2020-01-12 20:32:14 +0100

    definition of graph class as an Abstract Data Type

---
+++ BSF_pathfinder.py
@@ -0,0 +1,31 @@
+# define the graph class
+class Graph():
+
+    # initialization
+    def __init__ (self):
+        pass
+
+    # define nodes as property
+    def nodes(self):
+        pass
+
+    # define edges as property
+    def edges(self):
+        pass
+
+    # define adding the node as method
+    def add_node(self, node):
+        pass
+
+    # define removing the node as method
+    def rem_node(self, node):
+        pass
+
+    # define adding the edge between 2 nodes and assing the cost
+    def add_edge(self, node1, node2, cost):
+        pass
+
+    # define removing the edge between 2 nodes
+    def rem_edge(self, node1, node2):
+        pass
+
