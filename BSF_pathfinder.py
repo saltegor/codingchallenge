4abd099f9e0b75c05a5fd1838e44f90ce90d92b3
Egor Saltykov <saltegor@gmail.com>
2020-01-13 17:37:38 +0100

    filled and tested methods to add and remove the edge

--- BSF_pathfinder.py
+++ BSF_pathfinder.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 # define the graph class
 class Graph():
 
@@ -5,20 +6,11 @@ class Graph():
     def __init__ (self, graph_data):
         self.graph_data = graph_data
         # just checking 
-        print(self.graph_data)
-        # create the variable list for nodes
+        # print(self.graph_data)
+        # create the variable list for nodes, edges and cost
         self.nodes = []
         self.edges = []
-
-    # define nodes as property
-    #@property
-    #def nodes(self):
-    #    pass
-
-    # define edges as property
-    #@property
-    #def edges(self):
-    #    print(self.edges)
+        self.costs = []
 
     # define adding the node as method
     def add_node(self, node):
@@ -41,7 +33,7 @@ class Graph():
     def import_nodes(self):
         # import the nodes from the graph data
         for node in self.graph_data:
-            # check that the node doesn't repeat
+            # check that the node doesn't repeat 
             if node not in self.nodes:
                 # add the node to the nodes list at the end
                 self.nodes.append(str(node))
@@ -54,12 +46,23 @@ class Graph():
         return self.nodes
 
     # define adding the edge between 2 nodes and assing the cost
-    def add_edge(self, node1, node2, cost):
-        pass
+    def add_edge(self, node1, node2, cost = 1):
+        # add the edge to the edges list
+        self.edges.append([str(node1), str(node2)])
+        # get the index of the edge to assign the cost
+        index = self.edges.index([str(node1), str(node2)])
+        # assign the cost
+        self.costs.insert(index,cost)
+        print(self.edges)
+        print(self.costs)
 
     # define removing the edge between 2 nodes
     def rem_edge(self, node1, node2):
-        pass
+        index = self.edges.index([str(node1), str(node2)])
+        self.costs.pop(index)
+        print(self.costs)
+        self.edges.remove([node1,node2])
+        print(self.edges)
 
 graph = Graph([1,2,'r',3,4,'t',5,6])
 print(graph.import_nodes())
@@ -78,3 +81,6 @@ graph.rem_node(1)
 print(graph.get_nodes())
 graph.rem_node('t')
 print(graph.get_nodes())
+graph.add_edge('1','2', 5)
+graph.add_edge('2','1', 1)
+graph.rem_edge('1','2')
